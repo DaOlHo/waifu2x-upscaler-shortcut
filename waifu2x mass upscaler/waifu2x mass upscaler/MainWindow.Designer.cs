4526c97f971dfcee7d43f980b54a35b1fb5939e0
Danole <44514463+DaOlHo@users.noreply.github.com>
2020-03-21 07:27:26 -0700

    Commit project

---
+++ waifu2x mass upscaler/waifu2x mass upscaler/MainWindow.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace waifu2x_mass_upscaler
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpscaleButton = new System.Windows.Forms.Button();
+            this.UpscaleProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // UpscaleButton
+            // 
+            this.UpscaleButton.Location = new System.Drawing.Point(12, 12);
+            this.UpscaleButton.Name = "UpscaleButton";
+            this.UpscaleButton.Size = new System.Drawing.Size(255, 27);
+            this.UpscaleButton.TabIndex = 0;
+            this.UpscaleButton.Text = "Upscale!";
+            this.UpscaleButton.UseVisualStyleBackColor = true;
+            this.UpscaleButton.Click += new System.EventHandler(this.UpscaleButton_Click);
+            // 
+            // UpscaleProgressBar
+            // 
+            this.UpscaleProgressBar.Location = new System.Drawing.Point(12, 45);
+            this.UpscaleProgressBar.Name = "UpscaleProgressBar";
+            this.UpscaleProgressBar.Size = new System.Drawing.Size(255, 23);
+            this.UpscaleProgressBar.TabIndex = 1;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 80);
+            this.Controls.Add(this.UpscaleProgressBar);
+            this.Controls.Add(this.UpscaleButton);
+            this.Name = "MainWindow";
+            this.Text = "Waifu2x mass upscaler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UpscaleButton;
+        private System.Windows.Forms.ProgressBar UpscaleProgressBar;
+    }
+}
+
---
+++ waifu2x mass upscaler/waifu2x mass upscaler/MainWindow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace waifu2x_mass_upscaler
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpscaleButton_Click(object sender, EventArgs e)
+        {
+            string[] fileEntries = Directory.GetFiles(@"..\to upscale");
+            UpscaleProgressBar.Maximum = fileEntries.Length;
+            UpscaleProgressBar.Value = 0;
+            foreach (string fileName in fileEntries)
+            {
+                if (fileName.EndsWith(".jpg") || fileName.EndsWith(".jpeg") || fileName.EndsWith(".png"))
+                {
+                    Upscale(@"..\to upscale\" + fileName, 3, 2);
+                }
+                UpscaleProgressBar.PerformStep();
+            }
+        }
+
+        static void Upscale(string filePath, int noiseReductionLevel, int scaleLevel)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = @"waifu2x-ncnn-vulkan.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = $"-i \"{filePath}\" -o \"{filePath.Substring(0, filePath.LastIndexOf(".")) + " upscaled.png"}\" -n {noiseReductionLevel} -s {scaleLevel}";
+            Debug.WriteLine(startInfo.Arguments);
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
